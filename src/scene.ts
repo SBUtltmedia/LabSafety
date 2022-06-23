2e759b279cd92e9f8a687de97733e2724b27651d
Fletcher Ramee <fletcher.ramee@stonybrook.edu>
2022-06-23 13:23:16 -0400

    Add room, cylinder rotation, FPS camera

--- src/scene.ts
+++ src/scene.ts
@@ -3,18 +3,23 @@ import { HemisphericLight } from '@babylonjs/core/Lights/hemisphericLight';
 import { Vector3 } from '@babylonjs/core/Maths/math.vector';
 import { Color3, Color4 } from '@babylonjs/core/Maths/math.color';
 import { ArcRotateCamera } from '@babylonjs/core/Cameras/arcRotateCamera';
+import { UniversalCamera } from '@babylonjs/core/Cameras/universalCamera';
 import { SceneLoader } from '@babylonjs/core/Loading/sceneLoader';
 import { PointerDragBehavior } from '@babylonjs/core/Behaviors/Meshes/pointerDragBehavior';
 import { CreateGround } from '@babylonjs/core/Meshes/Builders/groundBuilder';
+import { CreateLines } from '@babylonjs/core/Meshes/Builders/linesBuilder';
 import { AbstractMesh } from '@babylonjs/core/Meshes/abstractMesh';
 import { Engine } from '@babylonjs/core/Engines/engine';
 import { StandardMaterial } from '@babylonjs/core/Materials/standardMaterial';
+import { Material } from '@babylonjs/core/Materials/material';
 import { BoundingInfo } from '@babylonjs/core/Culling/boundingInfo';
 import { Nullable } from '@babylonjs/core/types';
+import { LinesMesh } from '@babylonjs/core/Meshes/linesMesh';
 
 import '@babylonjs/loaders/glTF';  // To enable loading .glb meshes
 import '@babylonjs/core/Helpers/sceneHelpers';  // To enable creating the default XR experience
 import '@babylonjs/core/Rendering/boundingBoxRenderer';
+import '@babylonjs/core/Collisions/collisionCoordinator';
 
 
 function setBoundingInfoFromChildren(mesh: AbstractMesh): [Vector3, Vector3] {
@@ -27,7 +32,7 @@ function placeOnSurface(surface: AbstractMesh, ...meshes: AbstractMesh[]) {
     // Note: this function only changes the vertical position of the meshes, so a mesh may not be within the bounds of the surface.
     const surfaceLevel = surface.getBoundingInfo().boundingBox.maximum.y;
     meshes.forEach(mesh => {
-        const offset = mesh.position.y - mesh.getBoundingInfo().boundingBox.minimum.y;
+        const offset = mesh.position.y - mesh.getBoundingInfo().boundingBox.minimum.y + 0.01;  // The 0.01 is to prevent z-fighting; TODO: find a more elegant solution for this.
         mesh.position.y = surfaceLevel + offset;
     });
 }
@@ -40,10 +45,70 @@ function hideBoundingBoxes(...meshes: AbstractMesh[]) {
     meshes.forEach(mesh => mesh.showBoundingBox = false);
 }
 
+const g = -9.80665;  // Standard gravity
+function calculateCylinderPouringRotation(targetCylinder: AbstractMesh, pouringCylinder: AbstractMesh, r: number): Vector3 {
+    const targetCylinderBoundingBox = targetCylinder.getBoundingInfo().boundingBox;
+    const xTgt = (targetCylinderBoundingBox.minimumWorld.x + targetCylinderBoundingBox.maximumWorld.x) / 2;
+    const yTgt = targetCylinderBoundingBox.maximumWorld.y;
+
+    // Set the center of the pouring cylinder to be the origin. Variables oX and oY are poorly named; they should probably be xTgt and yTgt instead.
+    const oX = xTgt - pouringCylinder.position.x;
+    const oY = yTgt - pouringCylinder.position.y;
+    // const oXNorm = oX/Math.sqrt(oX**2 + oY**2);
+    const oYNorm = oY/Math.sqrt(oX**2 + oY**2);
+
+    const numerator = oX ** 2;
+    const denominator = 2 * r * (Math.asin(oYNorm) - r/2);
+    const quotient = numerator/denominator;  // TODO: why is this negative?
+    const root = Math.sqrt(Math.abs(quotient));
+    const theta = Math.acos(root) + Math.PI/2;  // Note the Math.PI/2 offset.
+    if (!Number.isFinite(theta) || Vector3.Distance(new Vector3(xTgt, yTgt, targetCylinder.position.z), pouringCylinder.position) < r) return Vector3.Zero();
+    if (pouringCylinder.position.x < targetCylinder.position.x) return new Vector3(0, 0, -theta);
+    return new Vector3(0, Math.PI, -theta);
+}
+
+// interface Options {
+//     points: Vector3[];
+//     updatable?: boolean | undefined;
+//     instance?: Nullable<LinesMesh> | undefined;
+//     colors?: Color4[] | undefined;
+//     useVertexAlpha?: boolean | undefined;
+//     material?: Material | undefined;
+// };
+// function showTrajectory(startPoint: Vector3, endPoint: Vector3, f: (x: number) => number, options: Options): void {
+//     const z = startPoint.z;
+//     const xs: number[] = [startPoint.x];
+//     const nLines = options.points.length - 1;
+//     const inc = (endPoint.x - startPoint.x)/nLines;
+//     for (let i = 1; i <= nLines - 1; ++i) {
+//         xs.push(startPoint.x + i*inc);
+//     }
+//     xs.push(endPoint.x);
+//     const points = xs.map(x => {
+//         const y = f(x);
+//         if (!Number.isFinite(y)) return new Vector3(x, x, z);
+//         return new Vector3(x, y, z)
+//     });
+//     options.points = points;
+//     console.log(`update points (nLines = ${nLines}): ${options.points.map(v => `(${v.x}, ${v.y})`)}`);
+//     CreateLines('trajectory', options);
+// }
+
 export const createScene = async (engine: Engine, canvas: HTMLCanvasElement) => {
     const scene = new Scene(engine);
     
-    const camera = new ArcRotateCamera('camera', -Math.PI/2, Math.PI / 2.5, 25, new Vector3(0, 0, 0), scene);
+    scene.gravity = new Vector3(0, g, 0);
+    scene.collisionsEnabled = true;
+
+    // const camera = new ArcRotateCamera('camera', -Math.PI/2, Math.PI / 2.5, 25, new Vector3(0, 0, 0), scene);
+    const camera = new UniversalCamera('camera', new Vector3(0, 15, -10), scene);
+    camera.keysUp.push(87);  // W
+    camera.keysDown.push(83);  // S
+    camera.keysLeft.push(65);  // A
+    camera.keysRight.push(68);  // D
+    camera.ellipsoid = new Vector3(1, 8, 1);
+    camera.checkCollisions = true;
+    camera.applyGravity = true;
     camera.attachControl(canvas, true);
 
     const light = new HemisphericLight('light', new Vector3(0, 1, 0), scene);
@@ -52,6 +117,7 @@ export const createScene = async (engine: Engine, canvas: HTMLCanvasElement) => 
     const groundMaterial = new StandardMaterial('ground-material', scene);
     groundMaterial.diffuseColor = new Color3(1 , 228/255, 196/255);
     ground.material = groundMaterial;
+    ground.checkCollisions = true;
 
     let cylinders: AbstractMesh[] = [];
     let table: Nullable<AbstractMesh> = null;
@@ -89,28 +155,97 @@ export const createScene = async (engine: Engine, canvas: HTMLCanvasElement) => 
         
         // Set positions
         cylinders.push(cylinder1, cylinder2, cylinderEmpty);
-        cylinders.forEach(c => console.log(c.rotation, c.rotationQuaternion));
         cylinder1.position.x += 5;
         cylinder2.position.x -= 5;
         cylinder1.rotationQuaternion = cylinder2.rotationQuaternion = cylinderEmpty.rotationQuaternion = null;
         cylinder1.rotation = new Vector3(0, Math.PI, 0);
         cylinder2.rotation = Vector3.Zero();
         cylinderEmpty.rotation = Vector3.Zero();
+        cylinders.forEach(cylinder => cylinder.getChildMeshes().find(mesh => mesh.name === 'BeakerwOpacity' || mesh.name === `${mesh.parent!.name}.BeakerwOpacity`)!.checkCollisions = true);
+        // showBoundingBoxes(...cylinders);
 
         if (table) placeOnSurface(table, ...cylinders);
 
         // Focus camera at the empty cylinder
-        camera.setTarget(cylinderEmpty);
+        // camera.setTarget(cylinderEmpty);
+        const r = cylinderEmpty.getBoundingInfo().boundingBox.maximum.y - cylinderEmpty.position.y;
+        // const options1: Options = {
+        //     points: new Array(10).fill(Vector3.Zero()),
+        //     updatable: true
+        // };
+        // const options2: Options = {
+        //     points: new Array(10).fill(Vector3.Zero()),
+        //     updatable: true
+        // };
+        cylinder1.rotationQuaternion = null;
+        cylinder2.rotationQuaternion = null;
+        cylinder1.rotation = new Vector3(0, 0, -Math.PI);
+        cylinder2.rotation = new Vector3(0, 0, -Math.PI / 2);
+        scene.registerBeforeRender(() => {
+            cylinder1.rotationQuaternion = null;
+            cylinder2.rotationQuaternion = null;
+            const cylinder1Rotation = calculateCylinderPouringRotation(cylinderEmpty, cylinder1, r);
+            const cylinder2Rotation = calculateCylinderPouringRotation(cylinderEmpty, cylinder2, r);
+            cylinder1.rotation.x = cylinder1Rotation.x;
+            cylinder1.rotation.y = cylinder1Rotation.y;
+            cylinder1.rotation.z += (cylinder1Rotation.z - cylinder1.rotation.z) / 10;
+            // cylinder1.rotation.addInPlace(cylinder1Rotation.subtract(cylinder1.rotation).divide(new Vector3(10, 10, 10)));
+            // cylinder1.rotation = cylinder1Rotation;
+            cylinder2.rotation.x = cylinder2Rotation.x;
+            cylinder2.rotation.y = cylinder2Rotation.y;
+            cylinder2.rotation.z += (cylinder2Rotation.z - cylinder2.rotation.z) / 10;
+            // cylinder2.rotation.addInPlace(cylinder2Rotation.subtract(cylinder2.rotation).divide(new Vector3(10, 10, 10)));
+            // cylinder2.rotation = cylinder2Rotation;
+
+            // function f(pouringCylinder: AbstractMesh, targetCylinder: AbstractMesh): (x: number) => number {
+            //     const theta = calculateCylinderPouringRotation(targetCylinder, pouringCylinder, r).z;
+                // if (theta <= Math.PI || theta >= 2*Math.PI) return [theta, x => x];
+            //     return function(x: number): number {
+            //         // const y = (x - pouringCylinder.position.x)**2/(2*Math.tan(theta)*(1-pouringCylinder.position.x)) + pouringCylinder.position.y;
+            //         const y = (Math.tan(theta) / (2 * r * Math.cos(theta))) * (x - pouringCylinder.position.x) ** 2 + (r / 2) * Math.sin(theta) + pouringCylinder.position.y;
+            //         return y;
+            //     };
+            // }
+            // const g = f(cylinder1, cylinderEmpty);
+            // const h = f(cylinder2, cylinderEmpty);
+            // showTrajectory(cylinder1.position, cylinderEmpty.position, g, options1);
+            // showTrajectory(cylinder2.position, cylinderEmpty.position, h, options2);
+        });
     });
 
-    SceneLoader.ImportMeshAsync('', '../models/', 'scienceLabInstructorTable.glb').then(result => {
-        table = result.meshes.find(mesh => mesh.name === '__root__')!;
+    // SceneLoader.ImportMeshAsync('', '../models/', 'scienceLabInstructorTable.glb').then(result => {
+    //     table = result.meshes.find(mesh => mesh.name === '__root__')!;
+    //     setBoundingInfoFromChildren(table);
+        
+    //     if (cylinders.length) placeOnSurface(table, ...cylinders);
+        
+    //     table.rotationQuaternion = null;
+    //     table.rotation.y = -Math.PI / 2;
+    // });
+    // const options: Options = {
+    //     points: [new Vector3(10, 20, 0), new Vector3(0, 20, 0)],
+    //     updatable: true,
+    //     instance: undefined
+    // };
+
+
+    SceneLoader.ImportMeshAsync('', '../models/', 'newRoomEnvironmentTLL.glb').then(result => {
+        const room = result.meshes.find(mesh => mesh.name === '__root__')!;
+        console.log(result);
+        table = result.meshes.find(mesh => mesh.name === 'Table.002')!;
+        const walls = result.meshes.find(mesh => mesh.name === 'Walls')!;
+        const cabinet = result.meshes.find(mesh => mesh.name === 'Cabinet')!;
+        setBoundingInfoFromChildren(room);
         setBoundingInfoFromChildren(table);
-        
         if (cylinders.length) placeOnSurface(table, ...cylinders);
-        
-        table.rotationQuaternion = null;
-        table.rotation.y = -Math.PI / 2;
+        room.rotationQuaternion = null;
+        room.rotation.y = -Math.PI / 2;
+        room.position.z -= 15;
+
+        table.checkCollisions = true;
+        walls.checkCollisions = true;
+        cabinet.checkCollisions = true;
+
     });
 
     const xr = await scene.createDefaultXRExperienceAsync();
