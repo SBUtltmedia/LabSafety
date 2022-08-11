d053c49fac81f8d2173a7aa094db610ccb8b1cc1
Fletcher Ramee <fletcher.ramee@stonybrook.edu>
2022-08-11 02:25:57 -0400

    Remove experimental clipboard from scene

--- src/scene.ts
+++ src/scene.ts
@@ -67,7 +67,7 @@ export const createScene = async (engine: Engine, canvas: HTMLCanvasElement) => 
     camera.checkCollisions = true;
 
 
-    Promise.all([loadCylinders(), loadRoom(),loadClipboard(scene)]).then(async ([cylinders, { root, table, walls, cabinet, floor }]) => {
+    Promise.all([loadCylinders(), loadRoom(), /* loadClipboard(scene) */]).then(async ([cylinders, { root, table, walls, cabinet, floor }]) => {
         camera.ellipsoid = new Vector3(0.4, 0.9, 0.4);
         camera.attachControl(canvas, true);
         camera.applyGravity = true;
