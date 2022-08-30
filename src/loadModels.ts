dd8970ab539cc18318532c8c12304c9cf09a8663
AlmondBread <pstdenis@gmail.com>
2022-08-30 17:52:13 -0400

    Made Clipboard load and positioned to begin testing sop24.svg

--- src/loadModels.ts
+++ src/loadModels.ts
@@ -6,28 +6,28 @@ import { AbstractMesh } from '@babylonjs/core/Meshes/abstractMesh';
 import {   Texture, StandardMaterial} from '@babylonjs/core';
 import { rootPath, GrabbableAbstractMesh } from './constants';
 
-export const loadModels = (models:[string]) =>
-Promise.all(models.map(model=>
-SceneLoader.ImportMeshAsync('', `${rootPath}models/`, model).then(result=>result.meshes.find(mesh => mesh.name === '__root__'))
-))
-
-
-
-// class model {
-//     fileName: string = 'table.glb';
-//     callback ? : Function;
-
-//     // Pass in this class as the required params
-//     constructor(params: model) {
-//         // object.assign will overwrite defaults if params exist
-//         Object.assign(this, params)
-//     }
-// }
-// export const loadModels = (models:[model]) =>
-// Promise.all(models.map(
-//     (currentmodel:model)=>{
-//         console.log(currentmodel)
-//         let {fileName,callback= (result:any)=>result}=currentmodel;
-// SceneLoader.ImportMeshAsync('', `${rootPath}models/`, fileName).then(result=>result)
-//     }
+// export const loadModels = (models:[string]) =>
+// Promise.all(models.map(model=>
+// SceneLoader.ImportMeshAsync('', `${rootPath}models/`, model).then(result=>result.meshes.find(mesh => mesh.name === '__root__'))
 // ))
+
+
+
+class model {
+    fileName: string = 'table.glb';
+    callback ? : Function;
+
+    // Pass in this class as the required params
+    constructor(params: model) {
+        // object.assign will overwrite defaults if params exist
+        Object.assign(this, params)
+    }
+}
+export const loadModels = (models:[model]) =>
+Promise.all(models.map(
+    (currentmodel:model)=>{
+        console.log(currentmodel)
+        let {fileName,callback= (result:any)=>result}=currentmodel;
+return SceneLoader.ImportMeshAsync('', `${rootPath}models/`, fileName).then(result=>result.meshes.find(mesh => mesh.name === '__root__'))
+    }
+))
