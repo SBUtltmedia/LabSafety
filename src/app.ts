29a4d7b815a6a0d0dbb005218e67c785682e63fc
Kairav Parekh <kpparekh@widow-tll.labs.stonybrook.edu>
2023-03-23 18:24:56 -0400

    Built with optimizations

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
-import "@babylonjs/core/Debug/debugLayer";
-import "@babylonjs/inspector";
+// import "@babylonjs/core/Debug/debugLayer";
+// import "@babylonjs/inspector";
 
 import { Scene } from '@babylonjs/core/scene';
 import { HemisphericLight } from '@babylonjs/core/Lights/hemisphericLight';
@@ -32,9 +32,7 @@ import '@babylonjs/core/Helpers/sceneHelpers';  // To enable creating the defaul
 import '@babylonjs/core/Collisions/collisionCoordinator';  // To enable collisions
 import '@babylonjs/core/Audio/audioSceneComponent';
 
-// console.log = function() {
-    
-// }
+console.log = () => {}
 
 class App {
     handAnimation: any;
