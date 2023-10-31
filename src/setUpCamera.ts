e7a4ef874f65add8ff671fa77391debe6d3ab274
Fletcher Ramee <fletcher.ramee@stonybrook.edu>
2023-10-31 06:53:22 -0400

    Implement camera setup function

--- src/setUpCamera.ts
+++ src/setUpCamera.ts
@@ -6,10 +6,7 @@ export function setUpCamera(camera: UniversalCamera): void {
     camera.ellipsoid = new Vector3(0.4, 0.7, 0.4);
     camera.applyGravity = true;
     camera.minZ = 0.0; // To prevent clipping through near meshes
-    camera.speed = 0;  // @todo: What is this doing here?
+    camera.speed = 0.16;
     camera.checkCollisions = true;
-    camera.keysUp.push(87); // W
-    camera.keysDown.push(83); // S
-    camera.keysLeft.push(65); // A
-    camera.keysRight.push(68); // D
+    camera.needMoveForGravity = false;
 }
