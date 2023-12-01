153405276fea6db5281e1c5834b80a7c4b38646d
Fletcher Ramee <fletcher.ramee@stonybrook.edu>
2023-12-01 13:15:00 -0500

    Clean up comments and imports

--- src/scene.ts
+++ src/scene.ts
@@ -1,26 +1,23 @@
 import { Engine } from "@babylonjs/core/Engines/engine";
-import { Scene } from "@babylonjs/core/scene";
-import { setUpScene } from "./setUpScene";
-import { UniversalCamera } from "@babylonjs/core/Cameras/universalCamera";
+import { HemisphericLight } from "@babylonjs/core/Lights/hemisphericLight";
+import { Light } from "@babylonjs/core/Lights/light";
 import { Vector3 } from "@babylonjs/core/Maths/math.vector";
 import { Mesh } from "@babylonjs/core/Meshes/mesh";
-import { setUpCamera } from "./setUpCamera";
+import { Scene } from "@babylonjs/core/scene";
+import { UniversalCamera } from "@babylonjs/core/Cameras/universalCamera";
+import { WebXRDefaultExperience, WebXRDefaultExperienceOptions } from "@babylonjs/core/XR/webXRDefaultExperience";
 
-import { loadMeshes } from "./loadMeshes";
-import { placeMeshes } from "./placeMeshes";
-import { placeCamera } from "./placeCamera";
-import { processMeshes } from "./processMeshes";
-import { SoundManager } from "./SoundManager";
-import { HemisphericLight } from "@babylonjs/core/Lights/hemisphericLight";
-import { Light, PointerEventTypes, WebXRDefaultExperience, WebXRDefaultExperienceOptions, WebXRState } from "@babylonjs/core";
-import { setUpXR } from "./setUpXR";
-import FlyToCameraBehavior from "./FlyToCameraBehavior";
-import { GUIManager } from "./GUIManager";
-import { PostSceneCylinder } from "./PostSceneCylinder";
-import { log } from "./utils";
 import { MAX_XR_GRAB_DISTANCE } from "./Constants";
-import { InteractionXRManager } from "./InteractionXRManager";
 import { enableTasks } from "./enableTasks";
+import { InteractionXRManager } from "./InteractionXRManager";
+import { loadMeshes } from "./loadMeshes";
+import { placeCamera } from "./placeCamera";
+import { placeMeshes } from "./placeMeshes";
+import { processMeshes } from "./processMeshes";
+import { setUpCamera } from "./setUpCamera";
+import { setUpScene } from "./setUpScene";
+import { setUpXR } from "./setUpXR";
+import { log } from "./utils";
 
 export let xrExperience: WebXRDefaultExperience;
 export let interactionXRManager: InteractionXRManager;
@@ -56,13 +53,11 @@ export async function createSceneAsync(engine: Engine): Promise<Scene> {
     xrExperience = await scene.createDefaultXRExperienceAsync(xrOptions);
     interactionXRManager = new InteractionXRManager(xrExperience);
     setUpXR(xrExperience);
-    // xrExperience.baseExperience.onStateChangedObservable.add(changePointerLock);
     
     const splashScreen = document.querySelector("div.splash");
     splashScreen.textContent = "Click to start!";
     splashScreen.addEventListener("click", () => {
         splashScreen.classList.add("hide");
-        // changePointerLock(xrExperience.baseExperience.state);
     }, false);
     log("createSceneAsync end WebXR initialization");
 
@@ -70,7 +65,7 @@ export async function createSceneAsync(engine: Engine): Promise<Scene> {
     // Results is an array of results, where each element is the result from importing a particular file.
     const results = await loadMeshes();
     // Flatten the mesh so we have a single-dimensional array of all the meshes in the scene.
-    // @todo: It might be that we don't need the results from loadMeshes() at all, because we could just retrieve the
+    // It might be that we don't need the results from loadMeshes() at all, because we could just retrieve the
     // meshes from the Scene object. I don't see much reason to do one in favor of the other.
     const meshes = results.map(result => result.meshes).flat() as Mesh[];
 
@@ -86,34 +81,7 @@ export async function createSceneAsync(engine: Engine): Promise<Scene> {
     xrExperience.teleportation.addFloorMesh(scene.getMeshByName("Floor"));
 
     fadeIn(light1);
-
-    // const cylinderMeshes = meshes.filter(mesh => mesh.name.split("-")[0] === "cylinder");
-    // const cylinderInstances = cylinderMeshes.map(mesh => )
-    // const xr = new XR(scene, xrExperience, null, cylinders, null, this.soundManager, this.fireExtinguisher);
-    // xr.addWebXr().then(addHandModels => {
-    //     if (xrExperience) {
-    //         const flyToCamera = new FlyToCameraBehavior(xrExperience.baseExperience);
-    //         const clipboard = scene.getMeshByName("clipboard");
-    //         clipboard.addBehavior(flyToCamera);
-    //     }
-    //     this.guiManager = new GUIManager(scene);
-    //     this.guiManager.createPromptWithButton("Welcome to the Lab Safety Simulation. Click on the clipboard to learn more about the simulation!");
-    //     xr.guiManager = this.guiManager;
-    //     xr.addHandModels = addHandModels;
-        
-    //     let desktopScene: PostSceneCylinder = new PostSceneCylinder(scene, cylinders, this.guiManager, soundManager, xrExperience, fireExtinguisher);
-    //     desktopScene.postSceneCylinder();
-    //     const toggleControllers = xr.addWebXrBehaviors();
-    // });
-
-    // @todo: Load and enable sounds.
-    // @todo: I like the SoundManager implementation in principle, but it seems like it's only really being used to load the sounds and then store them. That can easily be a function, so I think, for simplicity, it should be.
-    // I could be persuaded to implement a class if we had more complexity with sounds, but for now we don't. There may also be a Babylonian way to deal with sounds, but I don't know.
-    // log("createSceneAsync start sounds");
-    // const soundManager = new SoundManager([], scene);
-    // soundManager.enableAudio();
-    // await soundManager.loadSounds();
-    // log("createSceneAsync end sounds");
+    
     log("createSceneAsync enable tasks start");
     enableTasks(scene);
     log("createSceneAsync enable tasks end");
