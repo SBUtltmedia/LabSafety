a9acf8f4387c73a4a8deb821b0e2e974d6ceb24b
Fletcher Ramee <fletcher.ramee@stonybrook.edu>
2024-01-03 21:57:26 -0500

    Collapse XR hand root mesh

--- src/loadXRHands.ts
+++ src/loadXRHands.ts
@@ -1,23 +1,51 @@
 import { Mesh } from "@babylonjs/core/Meshes/mesh";
+
 import { loadMeshes } from "./loadMeshes";
 
 export async function loadXRHands(leftHandName: string, rightHandName: string): Promise<Mesh[]> {
-    const [leftHandResult, rightHandResult] = await loadMeshes([leftHandName, rightHandName]);
+    const results = await loadMeshes([leftHandName, rightHandName]);
 
-    // @todo: Error checking, or guarantee these will not be undefined
-    const leftHand = leftHandResult.meshes.find(mesh => mesh.id === leftHandName) as Mesh;
-    const rightHand = rightHandResult.meshes.find(mesh => mesh.id === rightHandName) as Mesh;
+    let leftHand: Mesh = null;
+    let rightHand: Mesh = null;
 
-    // Hand animations will play on a loop unless we pause them.
-    // Also name animation groups by handedness for later reference.
-    for (const animationGroup of leftHandResult.animationGroups) {
-        animationGroup.name += "-left";
-        animationGroup.pause();
+    for (const handResult of results) {
+        const rootMesh = handResult.meshes.find(mesh => mesh.id === leftHandName || mesh.id === rightHandName) as Mesh;
+
+        // Hand animations will play on a loop unless we pause them.
+        // Also name animation groups by associated mesh name for
+        // later reference.
+        for (const animationGroup of handResult.animationGroups) {
+            animationGroup.name += `-${rootMesh.name}`;
+            animationGroup.pause();
+        }
+
+        const [baseMesh] = rootMesh.getChildMeshes() as Mesh[];
+
+        if (!baseMesh) {
+            throw new Error(`Cannot collapse the root mesh of ${rootMesh.name}`);
+        }
+        
+        baseMesh.id = rootMesh.id;
+        baseMesh.name = rootMesh.name;
+
+        baseMesh.setParent(null);
+        rootMesh.dispose(true);
+
+        if (baseMesh.name === leftHandName) {
+            leftHand = baseMesh;
+        } else if (baseMesh.name === rightHandName) {
+            rightHand = baseMesh;
+        } else {
+            throw new Error("Base mesh name is invalid. This shouldn't be possible.");
+        }
     }
-    
-    for (const animationGroup of rightHandResult.animationGroups) {
-        animationGroup.name += "-right";
-        animationGroup.pause();
+
+    if (!leftHand) {
+        throw new Error("No left hand mesh found.");
+    }
+
+    if (!rightHand) {
+        throw new Error("No right hand mesh found.");
     }
 
     return [leftHand, rightHand];
