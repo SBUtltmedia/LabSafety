d8b861270511c7261306b5f4d5627387fcd5b907
Fletcher Ramee <fletcher.ramee@stonybrook.edu>
2024-01-03 20:24:49 -0500

    Process animation groups on loading XR hands

--- src/loadXRHands.ts
+++ src/loadXRHands.ts
@@ -1,15 +1,23 @@
 import { Mesh } from "@babylonjs/core/Meshes/mesh";
 import { loadMeshes } from "./loadMeshes";
 
-export async function loadXRHands(leftHandName: XRHandedness, rightHandName: XRHandedness): Promise<Mesh[]> {
-    const results = await loadMeshes([leftHandName, rightHandName]);
+export async function loadXRHands(leftHandName: string, rightHandName: string): Promise<Mesh[]> {
+    const [leftHandResult, rightHandResult] = await loadMeshes([leftHandName, rightHandName]);
 
     // @todo: Error checking, or guarantee these will not be undefined
-    let leftHand = results[0].meshes.find(mesh => mesh.id === leftHandName) as Mesh;
-    let rightHand = results[1].meshes.find(mesh => mesh.id === rightHandName) as Mesh;
-    if (!leftHand) {
-        leftHand = results[1].meshes.find(mesh => mesh.id === leftHandName) as Mesh;
-        rightHand = results[0].meshes.find(mesh => mesh.id === rightHandName) as Mesh;
+    const leftHand = leftHandResult.meshes.find(mesh => mesh.id === leftHandName) as Mesh;
+    const rightHand = rightHandResult.meshes.find(mesh => mesh.id === rightHandName) as Mesh;
+
+    // Hand animations will play on a loop unless we pause them.
+    // Also name animation groups by handedness for later reference.
+    for (const animationGroup of leftHandResult.animationGroups) {
+        animationGroup.name += "-left";
+        animationGroup.pause();
+    }
+    
+    for (const animationGroup of rightHandResult.animationGroups) {
+        animationGroup.name += "-right";
+        animationGroup.pause();
     }
 
     return [leftHand, rightHand];
