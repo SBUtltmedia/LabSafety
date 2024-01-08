e79004fba9e16fea62c48e8041aa1711c3f9acb3
Fletcher Ramee <fletcher.ramee@stonybrook.edu>
2024-01-08 15:17:57 -0500

    Snap grabbed meshes to hand position and orientation

--- src/InteractableBehavior.ts
+++ src/InteractableBehavior.ts
@@ -121,7 +121,15 @@ export class InteractableBehavior implements Behavior<Mesh> {
                         if (this.#moveAttached) {
                             switch (this.#grabState) {
                                 case GrabState.GRAB:
+                                    const previousRotation = this.mesh.rotation.clone();
                                     this.mesh.setParent(grabbingMesh);
+                                    this.mesh.position.copyFromFloats(0, 0, 0);
+
+                                    // This is necessary because setParent converts the local rotation to the grabbing mesh's space
+                                    // such that the grabbed mesh's absolute rotation remains the same. We don't want the absolute
+                                    // rotation to stay the same; we want the *local* rotation to stay the same. Therefore, we store
+                                    // it before parenting and restore it after parenting.
+                                    this.mesh.rotation.copyFrom(previousRotation);
                                     break;
                                 case GrabState.DROP:
                                     this.mesh.setParent(null);
