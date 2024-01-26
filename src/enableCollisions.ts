a88d9f520223873db3b6e35b5c942408a69fe36c
Kairav Parekh <kairav.parekh@outlook.com>
2024-01-25 20:06:35 -0500

    Enable collisions for the fume hood in the room

--- src/enableCollisions.ts
+++ src/enableCollisions.ts
@@ -1,6 +1,8 @@
 import { Mesh } from "@babylonjs/core/Meshes/mesh";
 
-export const COLLIDABLE_MESH_NAMES = ["WallsandFloor", "Floor", "Table", "Roof", "Countertop", "Walls"];
+export const COLLIDABLE_MESH_NAMES = ["WallsandFloor", "Floor", "Table", "Roof",
+                                      "Countertop", "Walls", "FumehoodBase_LP", "FumehoodInner_LP",
+                                      "FumehoodInnerHook_LP"];
 
 export function enableMeshCollisions(roomMesh: Mesh) {
     roomMesh.getChildMeshes().forEach(childMesh => {
