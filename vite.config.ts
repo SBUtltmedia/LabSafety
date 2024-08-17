84c052ad0f0e561ecf94f695613c2df07758b87a
Kairav Parekh <kairav.parekh@outlook.com>
2024-08-17 11:33:14 -0400

    Update base v2

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({
-  base:"/LabSafety/",
+  base:"/LabSafety-v2/",
 
   plugins: [splitVendorChunkPlugin(), mkcert()],
   server: {
