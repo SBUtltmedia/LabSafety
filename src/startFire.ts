572193daf6e8c7a3f85744e000ee416e1cada557
priya <pprkash2017@gmail.com>
2024-09-04 19:34:50 -0400

    Fix fire extinguisher

--- src/startFire.ts
+++ src/startFire.ts
@@ -8,14 +8,25 @@ import { FireBehavior } from "./FireBehavior";
 export function startFire(scene: Scene): Mesh {
 
     const fireMaterial = new FireMaterial("fire");
+    let emitter= MeshBuilder.CreateBox("emitter", {size: 0.5})
+
+    emitter.parent = scene.getMeshById("ExitDoor");
 
     ParticleHelper.CreateAsync("fire", scene).then((set) => {
-        let emitter= MeshBuilder.CreateBox("emitter", {size: 0.5})
         emitter.isVisible=false
-        emitter.position= new Vector3(-4,3,-1.6)
+        // emitter.position= new Vector3(-4,3,-1.6)
         const fireBehavior = new FireBehavior();
         emitter.addBehavior(fireBehavior);        
         set.emitterNode=emitter;
-        set.start();
+
+        fireBehavior.onFireObservable.add((isRunning) => {
+            if (!isRunning) {
+                set.dispose();
+            } else {
+                set.start();
+            }
+        })
     });
+
+    return emitter;
 }
