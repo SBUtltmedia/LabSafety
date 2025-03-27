d911287f8ba6bf183da0dea4f2c052e9bab11158
Kairav Parekh <kairav.parekh@outlook.com>
2025-03-27 18:27:49 -0400

    Fix fire extinguisher and hotspots

--- src/SmokeParticles.ts
+++ src/SmokeParticles.ts
@@ -3,6 +3,8 @@ import { GPUParticleSystem } from "@babylonjs/core/Particles/gpuParticleSystem";
 import { ParticleSystem } from "@babylonjs/core/Particles/particleSystem";
 import { Vector3 } from "@babylonjs/core/Maths/math.vector";
 import { Texture } from "@babylonjs/core/Materials/Textures/texture";
+import { Color4, MeshBuilder } from "@babylonjs/core";
+import GUI from 'lil-gui'; 
 
 export class SmokeParticles {
     particleSystem: ParticleSystem | GPUParticleSystem;
@@ -21,43 +23,75 @@ export class SmokeParticles {
 
         if (GPUParticleSystem.IsSupported) {
             console.log("Using the GPU for particle system")
-            particleSystem = new ParticleSystem("particles", 15000, scene);
+            particleSystem = new ParticleSystem("particles", 150000, scene);
         } else {
-            particleSystem = new ParticleSystem("particles", 10000 , scene);
+            particleSystem = new ParticleSystem("particles", 100000 , scene);
         }
 
-        particleSystem.particleTexture = new Texture("images/smokeParticleTexture.png", scene);
+        particleSystem.particleTexture = new Texture("images/smokeParticleTexture2.png", scene);
     
-        let x = 0.25, y = 1, z = x;
+        let x = 1, y = 1.5, z = x;
+
+        particleSystem.color1 = new Color4(0.7,0.7,0.7, 0.3);
+        particleSystem.color2 = particleSystem.color1;
+        particleSystem.colorDead = new Color4(1,1,1, 0);
+
+        particleSystem.renderingGroupId = 1;
+
 
         particleSystem.createPointEmitter(
             new Vector3(x, y, -z),
             new Vector3(-x, -y, z)
         )
 
-        particleSystem.emitRate = 5000;
+        particleSystem.emitRate = 15000;
+
+        const gui = new GUI();
 
 
         // // The particle system is now in the local space of the source mesh. Without this, the rotation and the position of the particle system would not change
         // // along with the source mesh (fire extinguisher in this case).
-        particleSystem.isLocal = true;
+        let props = {
 
-        particleSystem.gravity = new Vector3(0, 0, 100);
+        };
+        let gravity = new Vector3(0, 0, 90)
+       let obj = { isLocal : true,
+
+        gravity,
 
         // how long before the particles dispose
-        particleSystem.minLifeTime = 0.7;
-        particleSystem.maxLifeTime = 0.7;
+        minLifeTime : 0.2,
+        maxLifeTime : 1,
 
-        particleSystem.minSize = 0.05;
-        particleSystem.maxSize = 0.35;
+        minSize : 0.05,
+        maxSize : 0.05
+        }
+
+        let emitterSphere = MeshBuilder.CreateSphere("emitterSphere", {diameter: 0.05})
+        emitterSphere.parent = this.#mesh.getChildMeshes().find(mesh => mesh.name === "Hose");
+        console.log("hose: ", this.#mesh.getChildMeshes().find(mesh => mesh.name === "Hose"))
+
+        emitterSphere.position = new Vector3(0, 0.08, 0);
+        // emitterSphere.rotation = new Vector3(0, Math.PI/2, 0);
+
+        emitterSphere.isVisible = false;
+
+        for (let entry of Object.entries(obj)) {
+            // @ts-ignore
+            particleSystem[entry[0]] = entry[1];
+            // gui.add(particleSystem, entry[0]);
+        }
+        // gui.add(particleSystem, obj["gravity"].x);
+        console.log(Object.entries(particleSystem));
 
 
         particleSystem.blendMode = ParticleSystem.BLENDMODE_STANDARD;
 
-        particleSystem.emitter = this.#mesh as AbstractMesh;
+        particleSystem.emitter = emitterSphere;
         particleSystem.emitter.position.z += 0.1;
 
         this.particleSystem = particleSystem;
+        // this.start();
     }
 
     start() {
--- src/startFire.ts
+++ src/startFire.ts
@@ -1,6 +1,6 @@
 import { MeshBuilder } from "@babylonjs/core/Meshes/meshBuilder";
 import { Mesh } from "@babylonjs/core/Meshes/mesh";
-import { Scene, ParticleHelper, Vector3 } from "@babylonjs/core";
+import { Scene, ParticleHelper, Vector3, StandardMaterial, Color3 } from "@babylonjs/core";
 import { FireBehavior } from "./FireBehavior";
 import { AdvancedDynamicTexture } from "@babylonjs/gui/2D/advancedDynamicTexture";
 import { Ellipse } from "@babylonjs/gui";
@@ -9,14 +9,24 @@ export const createGUIElement = (mesh: Mesh) => {
     let adt = AdvancedDynamicTexture.CreateForMesh(mesh);
     let c1 = new Ellipse();
 
-    c1.width = 2;
-    c1.height = 2;
-    c1.color = "black";
-    c1.background = "#00aaff";
-    c1.thickness = 2;
+    c1.width = "100%"
+    c1.height = "100%";
+    c1.color = "Orange";
+    c1.thickness = 40;
+    c1.background = "green";
     c1.alpha = 0.9;
 
     adt.addControl(c1);
+}
+
+export const createHotspot = (name: string, pos: Vector3, scene: Scene) => {
+    let hotspot1 = MeshBuilder.CreatePlane(name, {size: 0.25});
+    hotspot1.parent = scene.getMeshById("ExitDoor");
+    hotspot1.isVisible = false;
+    hotspot1.position = pos;
+    hotspot1.rotation = new Vector3(0, Math.PI / 2, 0);
+
+    return hotspot1;
 }
 
 export function setupFires(scene: Scene): Mesh[] {
@@ -25,7 +35,6 @@ export function setupFires(scene: Scene): Mesh[] {
     emitters.push(MeshBuilder.CreateBox("emitter1", {size: 0.5}));
     emitters.push(MeshBuilder.CreateBox("emitter2", {size: 0.5}));
     emitters.push(MeshBuilder.CreateBox("emitter3", {size: 0.5}));
-    emitters.push(MeshBuilder.CreateBox("emitter4", {size: 0.5}));
 
     for (let emitter of emitters) {
         emitter.parent = scene.getMeshById("ExitDoor");    
@@ -34,22 +43,10 @@ export function setupFires(scene: Scene): Mesh[] {
     emitters[0].position.copyFromFloats(0,0,0);
     emitters[1].position.copyFromFloats(0,0,2.5);
     emitters[2].position.copyFromFloats(-2,0,4);
-    emitters[3].position.copyFromFloats(-4,0,4);
 
-    let hotspot1 = MeshBuilder.CreateSphere("hotspot1", {diameter: 0.45});
-    hotspot1.parent = scene.getMeshById("ExitDoor");
-    hotspot1.isVisible = false;
-    hotspot1.position = new Vector3(0, -0.5, -0.8);
-
-    let hotspot2 = MeshBuilder.CreateSphere("hotspot2", {diameter: 0.45});
-    hotspot2.parent = scene.getMeshById("ExitDoor");
-    hotspot2.isVisible = false;
-    hotspot2.position = new Vector3(0, -0.5, -0.2);
-
-    let hotspot3 = MeshBuilder.CreateSphere("hotspot3", {diameter: 0.45});
-    hotspot3.parent = scene.getMeshById("ExitDoor");
-    hotspot3.isVisible = false;
-    hotspot3.position = new Vector3(0, -0.5, 0.4);    
+    let hotspot1 = createHotspot("hotspot1", new Vector3(-0.1, -0.5, -0.8), scene);
+    let hotspot2 = createHotspot("hotspot2", new Vector3(-0.1, -0.5, -0.2), scene);
+    let hotspot3 = createHotspot("hotspot3", new Vector3(-0.1, -0.5, 0.4), scene);
     
     createGUIElement(hotspot1);
     createGUIElement(hotspot2);
